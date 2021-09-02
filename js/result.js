6443d4dc29501b191851f78c979e4fd5ecaf6e7d
Md-Sefat-Alam <md.sefatalam@gmail.com>
2021-09-02 17:17:09 +0600

    error handelling some design cahanged

--- js/result.js
+++ js/result.js
@@ -2,7 +2,13 @@ const searchFieldResult = document.getElementById('searchField-Result');
 const resultStatus = document.getElementById('resultStatus');
 const searchResultContainer = document.getElementById('searchResultContainer')
 
-const loadServerData = () => {
+const loadServerData = (getReturn = false) => {
+    loader('start')
+    displayOrNot('searchResultContainer', 'none');
+    displayOrNot('resultStatus', 'none');
+
+
+
     const invalidSearchTextResult = document.getElementById('invalidSearchTextResult');
     const searchFieldText = searchFieldResult.value;
 
@@ -14,16 +20,28 @@ const loadServerData = () => {
                 showData(data);
                 console.log(data)
 
-            });
-    } else {
-        invalidSearchTextResult.innerText = 'Enter Search Text'
+            })
     }
-    searchFieldResult.addEventListener('keyup', () => {
-        invalidSearchTextResult.innerText = '';
-    })
+    else {
+        loader('stop');
+        invalidSearchTextResult.innerText = 'Enter Search Text';
+        return;
+    }
 }
 
+searchFieldResult.addEventListener('keyup', () => {
+    invalidSearchTextResult.innerText = '';
+    loader('stop');
+    return;
+})
+
+
+
 const showData = serverData => {
+    loader('stop');
+    displayOrNot('searchResultContainer', 'grid')
+    displayOrNot('resultStatus', 'flex');
+
     searchResultContainer.textContent = "";
     resultStatus.style.display = 'flex'     //showing search status
     const totalResultStatus = document.getElementById('totalResult');
@@ -39,9 +57,6 @@ const showData = serverData => {
     else {
         searchResultContainer.innerHTML = `<p>No result found for <span style="color: red;">"${serverData.q}"</span></p>`
     }
-
-
-
 }
 const setResultData = (serverData) => {
     //showing search result
@@ -87,7 +102,34 @@ const setResultData = (serverData) => {
 const author_name = (names) => {
     let author_name = ''
     for (const name of names) {
+        //fixed showing publisher
+        if (name === names[5]) {
+            author_name += name + `<span style='color: green;'>...more ${names.length - 5}</span>`;
+            return author_name;
+        }
         author_name += name + ", "
     }
     return author_name;
+}
+
+
+//spinner show or hidden by function
+const loader = loadingStatus => {
+    const loaderElement = document.getElementById('loader')
+    if (loadingStatus === 'start') {
+        loaderElement.style.display = 'block';
+    }
+    else {
+        loaderElement.style.display = 'none';
+    }
+}
+
+//manage display or not
+const displayOrNot = (propertyId, displayValue = 'block') => {
+    const displayElement = document.getElementById(propertyId);
+    if (displayValue === 'none') {
+        displayElement.style.display = 'none';
+    } else {
+        displayElement.style.display = displayValue;
+    }
 }
